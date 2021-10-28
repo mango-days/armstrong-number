c4de892070f4f61315d711b3f38e3a32a4a1ee7a
mango-days <77330707+mango-days@users.noreply.github.com>
2021-10-28 20:48:31 +0530

    Update armstrong.py

--- armstrong.py
+++ armstrong.py
@@ -1,14 +1,7 @@
-def size(number):
-  index = 0
-  while number!=0:
-    number = int(number/10)
-    index+=1
-  return index
-
 number=int(input())
 number_copy=number
 ans=0
-index=size(number)
+index=len(str(number))
 while number!=0:
   temp=number%10
   ans+=pow(temp,index)
